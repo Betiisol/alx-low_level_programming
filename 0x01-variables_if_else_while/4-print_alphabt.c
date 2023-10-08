68d674fa9a7fb5b3f6518d4eac01592b9b02320b
Betiisol <betiisolomon61@gmail.com>
2023-10-08 12:21:53 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * main - Prints the lower alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
